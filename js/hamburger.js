2543755140bf2a8a93ad46c5bb7d89dfa3459019
IgDazza <135366045+IgDazza@users.noreply.github.com>
2023-06-23 16:25:48 -0300

    final 1.0

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,13 @@
 const hamburguesa = document.querySelector(".hamburguesa");
 const nav = document.querySelector(".navigator");
+const drop = document.querySelector(".drop")
 
 hamburguesa.addEventListener("click", () => {
     hamburguesa.classList.toggle("active");
     nav.classList.toggle("active");
 
+})
+
+drop.addEventListener("click", () => {
+    drop.classList.toggle("active");
 })
